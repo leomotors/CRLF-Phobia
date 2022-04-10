9e475c488dcc00482f45edf2bf8c65d8a75c883d
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2022-04-10 22:00:25 +0700

    [Release] 0.1.0 and workflow test

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # CRLF-Phobia
 
+[![](https://img.shields.io/npm/v/crlf-phobia.svg?maxAge=3600)](https://www.npmjs.com/package/crlf-phobia)
+ [![](https://img.shields.io/npm/dt/crlf-phobia.svg?maxAge=3600)](https://www.npmjs.com/package/crlf-phobia)
+
 CRLF can cause disaster on your app sometimes.
 
 Run this simple inefficient app to scan your project files and it will warn
@@ -17,7 +20,7 @@ Usage:
 crlf-phobia *.cpp
 ```
 
-*Note*: CRLF-Phobia use your shell to glob files
+*Note*: CRLF-Phobia support both shell glob (Glob done by shell before parsing argv into program) and also shipped with glob ~~in case your shell is lazy.~~
 
 ### Q: Isn't there already an npm package that can do this?
 
@@ -27,10 +30,13 @@ crlf-phobia *.cpp
 
 ### A: Colors and MEMES (~~and less efficient~~ to be fixed)
 
+## Limitation (compared to existing)
+
+- Awful performance: This library check for whole file to make sure **not**
+a *single CRLF can survive!*
+
+- Cannot only detect CRLF and LF, because who use CR nowadays?
+
 ## TODO
 
-- Make it efficient
-
-- Auto-Transform
-
-- Auto ignore gitignored files
+- Auto-Transform CRLF into LF
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { program } from "commander";
-
 import globby from "globby";
 import * as fs from "node:fs";
 import chalk from "chalk";
+import ignore from "ignore";
 
-program.parse();
+if (process.argv[2] == "--help" || process.argv[2] == "-h") {
+    console.log("crlf-phobia {glob regex}");
+}
 
 const files = globby.sync(process.argv.slice(2));
 
@@ -18,13 +19,24 @@ function shortenFileName(str: string, len: number) {
 }
 
 const maxFileLength = Math.min(
-    50,
+    80,
     Math.max(...files.map((file) => file.length))
 );
 
+const ignorelists = fs.existsSync(".gitignore")
+    ? fs
+          .readFileSync(".gitignore")
+          .toString()
+          .split("\n")
+          .filter((line) => line.length && !line.startsWith("#"))
+    : [];
+
+const ig = ignore().add(ignorelists);
+const filteredFiles = ig.filter(files);
+
 const crlflists: string[] = [];
 
-for (const file of files) {
+for (const file of filteredFiles) {
     const sfile = shortenFileName(file, 50);
 
     const isCRLF = fs
@@ -48,3 +60,5 @@ if (crlflists.length > 0) {
         console.log(chalk.red(file));
     }
 }
+
+process.exit(crlflists.length > 0 ? 1 : 0);
